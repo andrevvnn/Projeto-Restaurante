04a5a81a439993bde7cf4b78d409bf170830abb1
Andre Novaes <andre_v_n@hotmail.com>
2021-07-26 20:45:58 -0300

    Adicionando Classe Pratos

---
+++ src/model/Pratos.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Pratos {
+  private String nome;
+  private String tipo;
+  private int quantidade;
+  private int quantidadeemestoque;
+  
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getTipo() {
+	return tipo;
+}
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+public int getQuantidade() {
+	return quantidade;
+}
+public void setQuantidade(int quantidade) {
+	this.quantidade = quantidade;
+}
+public int getQuantidadeemestoque() {
+	return quantidadeemestoque;
+}
+public void setQuantidadeemestoque(int quantidadeemestoque) {
+	this.quantidadeemestoque = quantidadeemestoque;
+}
+  
+}
