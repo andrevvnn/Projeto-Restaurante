5280aef9279ac9c770b901f3d00a52a7ac5fa457
André Novaes <53873000+andrevvnn@users.noreply.github.com>
2021-07-26 21:04:17 -0300

    Atualizando Classe Pratos

--- src/model/Pratos.java
+++ src/model/Pratos.java
@@ -9,35 +9,35 @@ public class Pratos {
   private int quantidadeemestoque;
   private BigDecimal preco;
   
-public String getNome() {
-	return nome;
-}
-public BigDecimal getPreco() {
-	return preco;
-}
-public void setPreco(BigDecimal preco) {
-	this.preco = preco;
-}
-public void setNome(String nome) {
-	this.nome = nome;
-}
-public String getTipo() {
-	return tipo;
-}
-public void setTipo(String tipo) {
-	this.tipo = tipo;
-}
-public int getQuantidade() {
-	return quantidade;
-}
-public void setQuantidade(int quantidade) {
-	this.quantidade = quantidade;
-}
-public int getQuantidadeemestoque() {
-	return quantidadeemestoque;
-}
-public void setQuantidadeemestoque(int quantidadeemestoque) {
-	this.quantidadeemestoque = quantidadeemestoque;
-}
+	public String getNome() {
+		return nome;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public int getQuantidadeemestoque() {
+		return quantidadeemestoque;
+	}
+	public void setQuantidadeemestoque(int quantidadeemestoque) {
+		this.quantidadeemestoque = quantidadeemestoque;
+	}
   
 }
