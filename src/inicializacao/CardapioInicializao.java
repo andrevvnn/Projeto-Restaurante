d0b11b6d99d5ef41e488fbcbe5c2f8a43c4ade84
Andre Novaes <andre_v_n@hotmail.com>
2021-08-05 20:10:49 -0300

    Criando Classe de Inicialização

---
+++ src/inicializacao/CardapioInicializao.java
@@ -0,0 +1,19 @@
+package inicializacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Bebidas;
+
+public class CardapioInicializao {
+
+	
+	
+	public List<Bebidas> retornaCardapioBebidas (){
+		
+		
+		return null;
+	}
+	
+
+}
